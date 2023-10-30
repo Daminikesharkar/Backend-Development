ea1412dac7cf6e18dc3b270e1db5f80630e10556
Damini Kesharkar <daminisk272@gmail.com>
2023-10-30 14:15:14 +0530

    Cleaned up the code using module exports

--- SimpleServer/Server1/routes.js
+++ SimpleServer/Server1/routes.js
@@ -1,8 +1,6 @@
-const http = require("http")
 const fs = require("fs")
 
-const server = http.createServer((req,res)=>{
-
+const requestHandler = (req,res) =>{
     if(req.url === '/'){
         res.write('<html>')
         res.write('<head><title>server</title></head>')
@@ -20,7 +18,7 @@ const server = http.createServer((req,res)=>{
         return req.on('end',()=>{
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split("=")[1];
-            fs.writeFile('SimpleServer/message.txt',message,(err)=>{
+            fs.writeFile('SimpleServer/Server1/message.txt',message,(err)=>{
                 res.statusCode = 302;
                 res.setHeader('Location','/')
                 return res.end();
@@ -29,8 +27,14 @@ const server = http.createServer((req,res)=>{
         })  
 
     }
-})
+}
 
-server.listen(2000,()=>{
-    console.log("Server is live...")
-})
+module.exports = requestHandler;
+
+// module.exports = {
+//     handler: requestHandler,
+//     someText: "Some text"
+// }
+
+// exports.handler = requestHandler;
+// exports.someText = 'Some text'
